17fe9f35f86e44d52b007d5cf37975ecbbf673d8
beybora <bora.b.sarikaya@gmail.com>
2025-05-12 15:17:46 +0200

    add new routes, including a dynamic route for post detail page

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,23 @@ import Home from './pages/Home';
 import Layout from './pages/Layout';
 import Register from './pages/Auth/Register';
 import Login from './pages/Auth/Login';
+import { useContext } from 'react';
+import { AppContext } from './Context/AppContext';
+import Create from './pages/Posts/Create';
+import Show from './pages/Posts/Show';
 
 function App() {
+  const { user } = useContext(AppContext);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="login" element={<Login />} />
+          <Route path="/register" element={user ? <Home /> : <Register />} />
+          <Route path="/login" element={user ? <Home /> : <Login />} />
+          <Route path="/create" element={user ? <Create /> : <Login />} />
+
+          <Route path="/posts/:id" element={<Show />} />
         </Route>
       </Routes>
     </BrowserRouter>
