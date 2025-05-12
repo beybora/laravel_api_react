18be603c4f07a9dce96ecf97d72b186bcb15cc45
beybora <bora.b.sarikaya@gmail.com>
2025-05-12 15:19:45 +0200

    add login form with error handling and token management in Login componen

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,7 +1,66 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AppContext } from '../../Context/AppContext';
 
 const Login = () => {
-  return <h1 className="title">Login</h1>;
+  const navigate = useNavigate();
+  const { setToken } = useContext(AppContext);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const res = await fetch('api/login', {
+      method: 'post',
+      body: JSON.stringify(formData),
+    });
+
+    const data = await res.json();
+
+    if (data.errors) {
+      setErrors(data.errors);
+    } else {
+      localStorage.setItem('token', data.token);
+      setToken(data);
+      navigate('/');
+    }
+  };
+
+  return (
+    <>
+      <h1 className="title"> Login </h1>
+      <form onSubmit={handleLogin} className="w-1/2 mx-auto space-y-6">
+        <div>
+          <input
+            type="text"
+            placeholder="Email"
+            value={formData.email}
+            onChange={(e) =>
+              setFormData({ ...formData, email: e.target.value })
+            }
+          />
+          {errors.email && <p className="error">{errors.email}</p>}
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={(e) =>
+              setFormData({ ...formData, password: e.target.value })
+            }
+          />
+          {errors.password && <p className="error">{errors.password}</p>}
+        </div>
+        <button className="primary-btn">Login</button>
+      </form>
+    </>
+  );
 };
 
 export default Login;
