63c54ae3dfe7b2d762d0ea562196ea80f46bba6e
beybora <bora.b.sarikaya@gmail.com>
2025-05-12 15:20:40 +0200

    Add dynamic header with user state and logout functionality in Layout component

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
+import { AppContext } from '../Context/AppContext';
 
 const Layout = () => {
+  const { user, token, setUser, setToken } = useContext(AppContext);
+  const navigate = useNavigate();
+
+  const handleLogout = async (e) => {
+    e.preventDefault();
+
+    const res = await fetch('/api/logout', {
+      method: 'post',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = await res.json();
+
+    if (res.ok) {
+      setToken(null);
+      setUser(null);
+      localStorage.removeItem('token');
+      navigate('/');
+    }
+  };
+
   return (
     <>
       <header>
@@ -9,14 +33,27 @@ const Layout = () => {
           <Link to="/" className="nav-link">
             Home
           </Link>
-          <div className="space-x-4">
-            <Link to="/register" className="nav-link">
-              Register
-            </Link>
-            <Link to="/login" className="nav-link">
-              Login
-            </Link>
-          </div>
+
+          {user ? (
+            <div className="space-x-4 flex items-center">
+              <p className="text-white text-xs">User: {user.name}</p>
+              <Link to="/create" className="nav-link">
+                New Post
+              </Link>
+              <form onSubmit={handleLogout}>
+                <button className="nav-link">Logout</button>
+              </form>
+            </div>
+          ) : (
+            <div className="space-x-4">
+              <Link to="/register" className="nav-link">
+                Register
+              </Link>
+              <Link to="/login" className="nav-link">
+                Login
+              </Link>
+            </div>
+          )}
         </nav>
       </header>
 
