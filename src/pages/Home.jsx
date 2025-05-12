4acfa304da421cfd29234cad47ba1e4604eeca77
beybora <bora.b.sarikaya@gmail.com>
2025-05-12 15:17:25 +0200

    dd post listing functionality in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,50 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { AppContext } from '../Context/AppContext';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  const { name } = useContext(AppContext);
+  const [posts, setPosts] = useState([]);
+  const getPosts = async () => {
+    const res = await fetch('/api/posts');
+    const data = await res.json();
 
-  return <h1 className="title">Latest Posts: {name}</h1>;
+    if (res.ok) {
+      setPosts(data);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  return (
+    <>
+      <h1 className="title">Latest Posts:</h1>
+      {posts.length > 0 ? (
+        posts.map((post) => (
+          <div
+            className="mb-4 p-4 border rounded-md border-dashed border-slate-400"
+            key={post.id}
+          >
+            <div className="mb-2 flex items-start justify-between">
+              <div>
+                <h2>{post.title}</h2>
+              </div>
+              <Link
+                to={`/posts/${post.id}`}
+                className="bg-blue-500 text-white text-sm rounded-lg px-3 py-1"
+              >
+                Read more
+              </Link>
+            </div>
+            <p>{post.body}</p>
+          </div>
+        ))
+      ) : (
+        <p>There are no posts</p>
+      )}
+    </>
+  );
 };
 
 export default Home;
